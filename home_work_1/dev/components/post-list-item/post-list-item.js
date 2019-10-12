6a9986afeb8d272a52442533741486f9b60495a3
romaniukua <romaniukvadym1511@gmail.com>
2019-10-12 19:40:59 +0300

    bug with delete event has been fixed

--- home_work_1/dev/components/post-list-item/post-list-item.js
+++ home_work_1/dev/components/post-list-item/post-list-item.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const PostListItem = ({name, text, onDeleted}) => {
     return (
-        <li className="item" onClick = {onDeleted}>
+        <li className="item">
             <header className="item__head">
                 <h3 className="item__title">{name}</h3>
                 <div className="item__action">
                     <button className="button button--small">edit</button>
-                    <button className="button button--small">delete</button>
+                    <button className="button button--small" onClick = {onDeleted}>delete</button>
                 </div>
             </header>
             <p>{text}</p>
